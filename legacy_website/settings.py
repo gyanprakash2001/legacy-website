5926e8ec5409d8799d8c09860a44f4dc1c18589c
gyanprakash2001 <gyanprakashhh2001@gmail.com>
2025-10-03 10:42:20 +0530

    FIX: Added WhiteNoise middleware to settings.py for CSS serving

--- legacy_website/settings.py
+++ legacy_website/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ADDED WHITE NOISE HERE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
