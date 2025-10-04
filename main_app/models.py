a2e1bc3c51cc0b58e9d43362a4f5f37206530beb
gyanprakash2001 <gyanprakashhh2001@gmail.com>
2025-10-04 11:52:32 +0530

    FINAL DATA FIX: Increased max_length for College state field to resolve DataError

--- main_app/models.py
+++ main_app/models.py
@@ -80,7 +80,7 @@ class Follow(models.Model):
 
 class College(models.Model):
     name = models.CharField(max_length=500, unique=True)
-    state = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=200, null=True, blank=True)
     def __str__(self):
         return self.name
 
