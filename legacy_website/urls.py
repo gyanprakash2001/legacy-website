7d2f342539ddc2e878661e0bd4bd64c438f25c35
gyanprakash2001 <gyanprakashhh2001@gmail.com>
2025-10-04 19:36:28 +0530

    FINAL FIX: Removed conflicting local MEDIA_URL pattern in urls.py

--- legacy_website/urls.py
+++ legacy_website/urls.py
@@ -18,4 +18,3 @@ urlpatterns = [
 # from django.conf.urls.static import static
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
