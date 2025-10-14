49ce2a301c24179aba4f014b80d67ff80c16596b
gyanprakash2001 <gyanprakashhh2001@gmail.com>
2025-10-14 13:40:57 +0530

    FIX: Resolved NameError in project URLs by adding 'from main_app import views' for webhook.

--- legacy_website/urls.py
+++ legacy_website/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
